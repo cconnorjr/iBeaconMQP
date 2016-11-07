36bcbb28c43dc71fc5c28ab9c886e07a2617587b
cconnorjr <cconnorjr@gmail.com>
2016-11-07 15:34:58 -0500

    Added RSSI App
    Its called "Test"

---
+++ Android-SDK/MQP Project Files/MultiNotify/app/src/main/java/com/estimote/notification/MyApplication.java
@@ -0,0 +1,57 @@
+package com.estimote.notification;
+
+import android.app.Application;
+
+import com.estimote.notification.estimote.BeaconID;
+import com.estimote.notification.estimote.BeaconNotificationsManager;
+import com.estimote.sdk.EstimoteSDK;
+
+//
+// Running into any issues? Drop us an email to: contact@estimote.com
+//
+
+public class MyApplication extends Application {
+
+    private boolean beaconNotificationsEnabled = false;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // TODO: put your App ID and App Token here
+        // You can get them by adding your app on https://cloud.estimote.com/#/apps
+        EstimoteSDK.initialize(getApplicationContext(), "<#App ID#>", "<#App Token#>");
+
+        // uncomment to enable debug-level logging
+        // it's usually only a good idea when troubleshooting issues with the Estimote SDK
+//        EstimoteSDK.enableDebugLogging(true);
+    }
+
+    public void enableBeaconNotifications() {
+        if (beaconNotificationsEnabled) { return; }
+
+        BeaconNotificationsManager beaconNotificationsManager = new BeaconNotificationsManager(this);
+        //Beacon 1
+        beaconNotificationsManager.addNotification(
+                new BeaconID("B9407F30-F5F8-466E-AFF9-25556B57FE6D", 8897, 37240),
+                "Hello Beetroot",
+                "Goodbye Beetroot");
+        //Beacon 2
+        beaconNotificationsManager.addNotification(
+                new BeaconID("B9407F30-F5F8-466E-AFF9-25556B57FE6D", 20303, 16948),
+                "Hello Candy",
+                "Goodbye Candy");
+        //Beacon 3
+        beaconNotificationsManager.addNotification(
+                new BeaconID("B9407F30-F5F8-466E-AFF9-25556B57FE6D", 61665, 42057),
+                "Hello Lemon",
+                "Goodbye Lemon");
+        beaconNotificationsManager.startMonitoring();
+
+        beaconNotificationsEnabled = true;
+    }
+
+    public boolean isBeaconNotificationsEnabled() {
+        return beaconNotificationsEnabled;
+    }
+}
